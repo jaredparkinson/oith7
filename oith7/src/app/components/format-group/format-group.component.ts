747288d114a928ac93e448893d0541af4cb0d163
Jared Parkinson <jared@parkinson.im>
2019-11-07 02:05:27 -0700

    styles

--- oith7/src/app/components/format-group/format-group.component.ts
+++ oith7/src/app/components/format-group/format-group.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormatGroup } from '../../../../../oith-lib/src/models/Chapter';
+import { of, forkJoin } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { flatMap$ } from '../../../../../oith-lib/src/rx/flatMap$';
 
 @Component({
   selector: '[format-group]',
@@ -8,7 +11,41 @@ import { FormatGroup } from '../../../../../oith-lib/src/models/Chapter';
 })
 export class FormatGroupComponent implements OnInit {
   @Input() public formatGroup: FormatGroup;
+
+  public imgSrc?: string = '';
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    of(this.formatGroup)
+      .pipe(
+        filter(o => o !== undefined),
+        map(o => {
+          return forkJoin(this.setProps(o));
+        }),
+        flatMap$,
+      )
+      .subscribe(o => o);
+  }
+
+  public setProps(grp: FormatGroup) {
+    return of(grp).pipe(
+      filter(o => o.name !== undefined),
+      map(f => {
+        switch ((f.name as string).toLowerCase()) {
+          case 'img': {
+            if (f.attrs) {
+              f.attrs['src'] = `/assets/images/${f.attrs['src']}.jpg`;
+              console.log(f.attrs);
+              console.log(this.imgSrc);
+            }
+            break;
+          }
+
+          default:
+            break;
+        }
+      }),
+    );
+  }
 }
