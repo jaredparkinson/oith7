cda0c869914e1a87efdab873c85c0962d7beaa2d
Jared Parkinson <jared@parkinson.im>
2019-11-08 08:42:49 -0700

    features

--- oith7/src/app/services/link.service.ts
+++ oith7/src/app/services/link.service.ts
@@ -4,14 +4,32 @@ import { of } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { flatMap$ } from '../../../../oith-lib/src/rx/flatMap$';
+import { ChapterService } from './chapter.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LinkService {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private chapterService: ChapterService) {}
 
   public runLink(formatGroup: FormatGroup) {
+    console.log(formatGroup);
+    if (
+      formatGroup.attrs &&
+      formatGroup.attrs['href'] !== undefined &&
+      (formatGroup.attrs['href'] as string).includes('figure1_note_asterisk')
+    ) {
+      return scrollIntoView('[id*="figure1_note_asterisk"]').subscribe();
+    }
+
+    if (
+      formatGroup.attrs &&
+      formatGroup.attrs['href'] !== undefined &&
+      (formatGroup.attrs['href'] as string).includes('#p76') &&
+      this.chapterService.chapter.id.includes('js-h-1')
+    ) {
+      return scrollIntoView('.symbol').subscribe();
+    }
     return of(formatGroup)
       .pipe(
         filter(
@@ -28,4 +46,11 @@ export class LinkService {
       )
       .subscribe(o => console.log(o));
   }
+}
+
+export function scrollIntoView(selector: string) {
+  return of(document.querySelector(selector) as HTMLElement).pipe(
+    filter(o => o !== undefined),
+    map(o => o.scrollIntoView()),
+  );
 }
