ac183b9f79b2a1371f642f70c4fb408f556a0587
Jared Parkinson <jared@parkinson.im>
2019-11-15 12:31:06 -0700

    adding visibility

--- oith7/src/app/services/note-settings.service.ts
+++ oith7/src/app/services/note-settings.service.ts
@@ -12,6 +12,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../app.state';
 import { flatMap$ } from '../../../../oith-lib/src/rx/flatMap$';
 import { NoteSettings } from '../../../../oith-lib/src/processors/NoteSettings';
+import { Settings } from './init.service';
 
 @Injectable({
   providedIn: 'root',
@@ -26,28 +27,34 @@ export class NoteSettingsService {
     this.noteSettings$
       .pipe(
         map(() => {
-          return forkJoin(
-            this.store.select('settings').pipe(take(1)),
-            this.store.select('noteCategories').pipe(take(1)),
-            this.store.select('noteTypes').pipe(take(1)),
-            this.store.select('noteSettings').pipe(
-              take(1),
-              map(noteSettings => {
-                this.extractNoteSettings(noteSettings);
-              }),
-            ),
+          return this.store.select('settings').pipe(
+            take(1),
+            map(settings => {
+              return forkJoin(
+                this.store.select('noteCategories').pipe(take(1)),
+                this.store.select('noteTypes').pipe(take(1)),
+                this.store.select('noteSettings').pipe(
+                  take(1),
+                  map(noteSettings => {
+                    this.extractNoteSettings(noteSettings, settings);
+                  }),
+                ),
+              );
+            }),
           );
         }),
+        flatMap$,
         flatMap$,
       )
       .subscribe(o => console.log(o));
   }
 
-  private extractNoteSettings(noteSettings: NoteSettings) {
+  private extractNoteSettings(noteSettings: NoteSettings, settings: Settings) {
     const moreSetting = noteSettings.addSettings.find(addSettings =>
       addSettings.label.toLowerCase().includes('more'),
     );
     const more = moreSetting ? moreSetting.enabled : false;
+    console.log(settings);
 
     let onPlus = of(more).pipe(
       filter(o => o),
@@ -66,7 +73,13 @@ export class NoteSettingsService {
         .map(nS => nS.catOn.concat(nS.overlays)),
     ).pipe(flatMap$, flatMap$, distinctUntilChanged(), toArray());
     forkJoin(on.pipe(toArray()), onPlus.pipe(toArray()))
-      .pipe(flatMap$, flatMap$, flatMap$, toArray())
-      .subscribe(o => console.log(o));
+      .pipe(
+        flatMap$,
+        flatMap$,
+        flatMap$,
+        map(s => (settings.vis[s] = true)),
+        toArray(),
+      )
+      .subscribe(o => console.log(settings));
   }
 }
